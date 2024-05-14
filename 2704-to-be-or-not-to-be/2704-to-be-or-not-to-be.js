29dd28b6444cc03bd122ac1a43c98f70fe1a04f5
BhoomiAgrawal12 <167442415+BhoomiAgrawal12@users.noreply.github.com>
2024-05-14 10:42:05 +0530

    Time: 48 ms (77.40%), Space: 48.9 MB (38.40%) - LeetHub

--- 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -5,11 +5,13 @@
 var expect = function(val) {
     return {
         toBe: (val2) => {
-            if(val === val2) return true;
+            if(val === val2) 
+                return true;
             else throw new Error("Not Equal");
         },
         notToBe: (val2) => {
-            if(val !== val2) return true;
+            if(val !== val2)
+                return true;
             else throw new Error("Equal");
         }
     }
