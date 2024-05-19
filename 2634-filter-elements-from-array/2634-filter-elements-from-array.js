343c4f0e77fcc51fa884bb5fe82908a412e8702a
BhoomiAgrawal12 <167442415+BhoomiAgrawal12@users.noreply.github.com>
2024-05-19 18:10:55 +0530

    Time: 65 ms (5.95%), Space: 49.5 MB (7.11%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    var filteredarray=[];
+    for(var i=0;i<arr.length;i++){
+        if(fn(arr[i],i)){
+        filteredarray.push(arr[i]);
+    }
+    }
+    return filteredarray;
+};
