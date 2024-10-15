64fbce64c38e41043ab3d232a2a86a6c72b1cd8d
BhoomiAgrawal12 <167442415+BhoomiAgrawal12@users.noreply.github.com>
2024-10-15 22:44:20 +0530

    Time: 41 ms (10.95%), Space: 15.7 MB (45.68%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-         long long swap = 0;
-        int black = 0; 
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '0') 
-                swap += (long long) black; 
-            else
-                black++; 
+        long long cnt=0;
+        int black=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='0'){
+               cnt += (long long)black; 
+            }else black++;
         }
-        return swap;
+        return cnt;
     }
 };
