510765bb6106880d40dc5534989ace036daa70ed
BhoomiAgrawal12 <167442415+BhoomiAgrawal12@users.noreply.github.com>
2024-10-21 20:09:06 +0530

    Time: 8 ms (62.26%), Space: 9.8 MB (9.13%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int r=0,l=0,maxlen=0,fre=0,len=0;
+        int r=0,l=0,maxlen=0,fre=0;
         map<int,int>hash;
         while(r<s.size()){
             hash[s[r]-'A']++;
-             len=r-l+1;
             fre=max(fre, hash[s[r]-'A']);
-            if(len-fre>k){
+            if(r-l+1-fre>k){
                 hash[s[l]-'A']--;
                 l++;
                 fre=0;
             }
-                         len=r-l+1;
-            if(len-fre<=k){
-                maxlen=max(len,maxlen);
+            if(r-l+1-fre<=k){
+                maxlen=max(r-l+1,maxlen);
             }
             r++;
         }
